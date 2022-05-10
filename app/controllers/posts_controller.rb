5bf557bd92004ff02d96da75aa2e586ab5374699
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-05-10 11:39:50 -0400

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,15 @@
 class PostsController < ApplicationController
+   def index
+      posts = Post.all
+      render json: posts, status: :ok
+   end
    
+   def show
+      post = Post.find_by(id:params[:id])
+      if post
+         render json: post, status: :ok
+      else
+         render json: { error: "Post not Found" }, status: :not_found
+      end
+   end
 end
