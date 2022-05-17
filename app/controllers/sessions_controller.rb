b3eee79476b7cb14514ed645f283ae11e3b127da
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-05-17 13:47:32 -0400

    Added cookies to store current user's id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
        user = User.find_by(username: params[:username])
        if user && user.authenticate(params[:password])
         session[:user_id] = user.id
+        cookies[:user_id] = user.id
         render json: user, status: :created
        else
         render json: {error: {login: "invalid username or password"}}, status: :unauthorized
